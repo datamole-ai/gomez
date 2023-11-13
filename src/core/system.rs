bc35ea223b3b0cc892f1c10251c811d5833e20a1
Petr Nevyhoštěný <petr.nevyhosteny@gmail.com>
2023-11-13 15:22:29 +0100

    feat!: remove RepulsiveSystem type

--- src/core/system.rs
+++ src/core/system.rs
@@ -1,10 +1,9 @@
 use nalgebra::{
-    allocator::Allocator,
     storage::{Storage, StorageMut},
-    DefaultAllocator, Dynamic, IsContiguous, OVector, Vector,
+    Dynamic, IsContiguous, Vector,
 };
 
-use super::{base::Problem, domain::Domain};
+use super::base::Problem;
 
 /// The trait for defining equations systems.
 ///
@@ -73,79 +72,5 @@ pub trait System: Problem {
         let mut fx = x.clone_owned();
         self.eval(x, &mut fx);
         fx.norm()
-    }
-}
-
-/// A wrapper type for systems that implements a standard mechanism for
-/// repulsing solvers from solutions that have been already found and stored in
-/// the archive.
-///
-/// **WARNING:** This is currently noop as the repulsion mechanism has not been
-/// determined yet. But the technique is mentioned in [A Decomposition-based
-/// Differential Evolution with Reinitialization for Nonlinear Equations
-/// Systems](https://www.sciencedirect.com/science/article/abs/pii/S0950705119305933)
-/// or [Testing Nelder-Mead Based Repulsion Algorithms for Multiple Roots of
-/// Nonlinear Systems via a Two-Level Factorial Design of
-/// Experiments](https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0121844),
-/// for example.
-pub struct RepulsiveSystem<'f, F: System> {
-    f: &'f F,
-    archive: Vec<OVector<F::Field, Dynamic>>,
-}
-
-impl<'f, F: System> RepulsiveSystem<'f, F> {
-    /// Initializes the repulsive system by wrapping the original system.
-    pub fn new(f: &'f F) -> Self {
-        Self {
-            f,
-            archive: Vec::new(),
-        }
-    }
-
-    /// Add a found solution to the archive.
-    pub fn push(&mut self, root: OVector<F::Field, Dynamic>) {
-        self.archive.push(root);
-    }
-
-    /// Get the size of the archive.
-    pub fn len(&self) -> usize {
-        self.archive.len()
-    }
-
-    /// Determine whether the archive is empty.
-    pub fn is_empty(&self) -> bool {
-        self.archive.is_empty()
-    }
-
-    /// Unpack the archive which contains all solutions found.
-    pub fn unpack(self) -> Vec<OVector<F::Field, Dynamic>> {
-        self.archive
-    }
-}
-
-impl<'f, F: System> Problem for RepulsiveSystem<'f, F> {
-    type Field = F::Field;
-
-    fn domain(&self) -> Domain<Self::Field> {
-        self.f.domain()
-    }
-}
-
-impl<'f, F: System> System for RepulsiveSystem<'f, F>
-where
-    DefaultAllocator: Allocator<F::Field, Dynamic>,
-{
-    fn eval<Sx, Sfx>(
-        &self,
-        x: &Vector<Self::Field, Dynamic, Sx>,
-        fx: &mut Vector<Self::Field, Dynamic, Sfx>,
-    ) where
-        Sx: Storage<Self::Field, Dynamic> + IsContiguous,
-        Sfx: StorageMut<Self::Field, Dynamic>,
-    {
-        // TODO: RepulsiveSystem should adjust the residuals of the inner system
-        // such that solvers tend to go away from the roots stored in the
-        // archive.
-        self.f.eval(x, fx)
     }
 }
